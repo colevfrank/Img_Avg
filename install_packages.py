876c2f3ec9a012a7dd249a8a732ab2932efbe480
COLE FRANK <cfrank@MacBook-Pro.local>
2023-10-05 16:48:35 -0400

    cleaned up install packages .py file

--- install_packages.py
+++ install_packages.py
@@ -1,9 +1,5 @@
-"""
-echo "## active_line 2 ##"
-This script installs the necessary python packages Pillow and numpy needed for the image_average.py script.
-echo "## active_line 3 ##"
-"""
-echo "## active_line 4 ##"
-import os
-echo "## active_line 5 ##"
-os.system("pip install Pillow numpy")
+"""                                                                                                                                                                      
+  This script installs the necessary python packages Pillow and numpy needed for the image_average.py script.                                                                    
+  """                                                                                                                                                                            
+  import os                                                                                                                                                                      
+  os.system("pip install Pillow numpy")
