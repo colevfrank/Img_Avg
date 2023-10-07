6f6963a81c09c3119117f3ff7ae3322bb82db803
COLE FRANK <cfrank@MacBook-Pro.local>
2023-10-07 12:44:49 -0400

    indent edits to install_packages

--- install_packages.py
+++ install_packages.py
@@ -1,5 +1,5 @@
 """                                                                                                                                                                      
   This script installs the necessary python packages Pillow and numpy needed for the image_average.py script.                                                                    
   """                                                                                                                                                                            
-  import os                                                                                                                                                                      
-  os.system("pip install Pillow numpy")
+import os                                                                                                                                                                      
+os.system("pip install Pillow numpy")
